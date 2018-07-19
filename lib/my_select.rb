4fe65fc6b84ecb4a9026f0abab9faf125bf2272e
Trevor Jameson <tjameson15@gmail.com>
2018-07-19 12:06:06 +0000

    Done.

--- lib/my_select.rb
+++ lib/my_select.rb
@@ -1,3 +1,23 @@
+require 'pry'
+
 def my_select(collection)
- # your code here!
+ if collection.size == 0
+   return false
+ else
+   arr_clone = []
+   counter = 0
+   while counter < collection.length
+    if yield(collection[counter])
+      arr_clone << collection[counter]
+    end
+     counter += 1
+   end
+    return arr_clone
+ end
 end
+
+=begin
+my_select([1, 2, 3, 4, 5]) do |num|
+  num.even?
+end
+=end
